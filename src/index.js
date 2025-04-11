f2988e5fa6a9cdd5784a3027667ba20b2220106e
Claire Sawyers <clairesawyers@MacBook-Air.fritz.box>
2025-04-11 16:13:35 +1200

    Fix index.js error

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Find the root element
+const rootElement = document.getElementById('root');
+
+// Ensure the element exists before trying to render
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Could not find root element to mount React application");
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
